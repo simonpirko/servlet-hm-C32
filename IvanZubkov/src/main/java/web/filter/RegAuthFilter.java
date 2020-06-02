2cbc0409be17bcd222d9d3840af9400d15a366e7
Ivan Zubkov <you@example.com>
2020-06-02 14:23:26 +0300

    Добавлена папка IvanZubkov

---
+++ IvanZubkov/src/main/java/web/filter/RegAuthFilter.java
@@ -0,0 +1,27 @@
+package src.main.java.web.filter;
+
+import entity.User;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter (filterName = "regFilter", servletNames = {"RegServlet", "AuthServlet"})
+public class RegAuthFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        User user = (User) req.getSession().getAttribute("currentUser");
+        if (user == null) {
+            chain.doFilter(req, res);
+        } else {
+            req.setAttribute("message", "No action available");
+            getServletContext().getRequestDispatcher("/").forward(req, res);
+        }
+    }
+    }
+
---
+++ IvanZubkov/src/main/java/web/servlet/RegServlet.java
@@ -0,0 +1,37 @@
+package src.main.java.web.servlet;
+
+import entity.User;
+import storage.UserStorage;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/reg", name = "RegServlet")
+
+public class RegServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getServletContext().getRequestDispatcher("/pages/reg.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserStorage userStorage = new UserStorage();
+        String name = req.getParameter("name");
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        if (userStorage.checkUserByLogin(login)) {
+        userStorage.add(new User(login, name, password));
+        resp.sendRedirect("/");
+        }
+        else {
+            req.setAttribute("message", "Login can't be used");
+            getServletContext().getRequestDispatcher("/pages/reg.jsp").forward(req, resp);
+        }
+    }
+}
